365e2edee8086dca14940742224c7f2c0fa43cbb
Naveen Kumar <456endson@gmail.com>
2020-10-29 17:42:46 +0530

    Used the new bg svg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Header from "./Header";
-import bgLeft from "./images/bg-left.svg";
-import bgRight from "./images/bg-right.svg";
+import bg from "./images/bg.svg";
+import Hero from "./Hero";
 import "./styles/Root.css";
 
 const App = () => {
   return (
     <>
       <Header />
-      {/* <img src={bgRight} alt="" className="bg bg-right" />
-      <img src={bgLeft} alt="" className="bg bg-left" /> */}
+      <Hero />
+      <img src={bg} alt="" className="bg bg-right" />
+      {/* <img src={bg} alt="" className="bg bg-left" /> */}
     </>
   );
 };
